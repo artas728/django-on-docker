f0e51c13003fbe565d548d6cef2fc1359c9ae054
artas <mahatma728@gmail.com>
2020-12-06 13:26:12 -0300

    :sparkles: added new endpoints for tests

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url
+from upload import views
 from upload.views import image_upload
 
 urlpatterns = [
     path("", image_upload, name="upload"),
+    url(r'^save_to_redis/$', views.save_to_redis, name='save_to_redis'),
+    url(r'^sync_endpoint/$', views.endpoint, name='endpoint'),
+    url(r'^write_to_db/$', views.write_to_db, name='write_to_db'),
     path("admin/", admin.site.urls),
 ]
 
--- app/upload/views.py
+++ app/upload/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from django.http import JsonResponse
+from .models import TestModel
+import json
+import redis
+import time
 
+redis_cli = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+def save_to_redis(request):
+    data = json.loads(request.body.decode())
+    for key, value in data.items():
+        redis_cli.rpush(key, value)
+    return JsonResponse({'success': True, 'data': 'Saved in Redis'})
+
+def endpoint(request):
+    time.sleep(1)
+    return JsonResponse({'success': True, 'data': 'Request processed'})
+
+def write_to_db(request):
+    data = json.loads(request.body.decode())
+    for row in data:
+        TestModel.objects.create(key1=row['key1'],
+                                 key2=row['key2'],
+                                 key3=row['key3'],
+                                 key4=row['key4'])
+    return JsonResponse({'success': True, 'data': 'Data has been saved'})
 
 def image_upload(request):
     if request.method == "POST" and request.FILES["image_file"]:
